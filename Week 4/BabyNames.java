8b5842c4474929d3b3353b4197be59a3a7cca518
Xenia Shatova <98309459+Xenia-S@users.noreply.github.com>
2022-09-27 00:44:16 +0600

    Update BabyNames.java

--- Week 4/BabyNames.java
+++ Week 4/BabyNames.java
@@ -12,7 +12,6 @@ public class BabyNames {
         int girsNames = 0;
         for (CSVRecord rec : fr.getCSVParser(false)) {
             totalBirth += Integer.parseInt(rec.get(2));
-            //System.out.println(rec.get(1) + " " + Integer.parseInt(rec.get(2)));  // проверка
             if (rec.get(1).equals("M")) {
                 totalBoys += Integer.parseInt(rec.get(2));
                 boysNames += 1;
@@ -33,7 +32,6 @@ public class BabyNames {
         if (Integer.toString(year).length() < 4 || Integer.toString(year).length() > 4) {
             return -1; // если год вписан в неправильном формате, не будет ошибки
         }
-        // FileResource fr = new FileResource("us_babynames_test/yob" + year + "short.csv");
         FileResource fr = new FileResource("us_babynames_by_year/yob" + year + ".csv");
         int rank = 0;
         for (CSVRecord rec : fr.getCSVParser(false)) {
@@ -48,8 +46,6 @@ public class BabyNames {
     }
     
     public void testGetRank() {
-        
-        /*
         System.out.println(getRank(2012, "Emma", "F"));       // 2
         System.out.println(getRank(2012, "Emma", "M"));       // -1
         System.out.println(getRank(2013, "Noah", "M"));       // 1
@@ -60,7 +56,6 @@ public class BabyNames {
         System.out.println(getRank(14, "William", "M"));      // -1
         System.out.println(getRank(141414, "William", "M"));  // -1
         System.out.println(getRank(2012, "Ema", "F"));        // -1
-        */
         
         System.out.println(getRank(1960, "Emily", "F"));
         System.out.println(getRank(1971, "Frank", "M"));
@@ -70,7 +65,6 @@ public class BabyNames {
     public String getName(int year, int rank, String gender) {
         String name = "NO NAME";
         int count = 0;
-        // FileResource fr = new FileResource("us_babynames_test/yob" + year + "short.csv");
         FileResource fr = new FileResource("us_babynames_by_year/yob" + year + ".csv");
         for (CSVRecord rec : fr.getCSVParser(false)) {
             if (rec.get(1).equals(gender)) {
@@ -84,7 +78,6 @@ public class BabyNames {
     }
     
     public void testGetName() {
-        /*
         System.out.println(getName(2012, 1, "M")); // Jacob
         System.out.println(getName(2012, 2, "F")); // Emma
         System.out.println(getName(2012, 3, "M")); // Ethan
@@ -93,7 +86,6 @@ public class BabyNames {
         System.out.println(getName(2013, 5, "F")); // Ava
         System.out.println(getName(2014, 4, "M")); // Jacob
         System.out.println(getName(2014, 6, "F")); // NO NAME
-        */
        
        System.out.println(getName(1980, 350, "F"));
        System.out.println(getName(1982, 450, "M"));
@@ -111,14 +103,13 @@ public class BabyNames {
     }
     
     public void testWhatIsNameInYear() {
-        /*
         whatIsNameInYear("Sophia", 2012, 2013, "F"); // Sophia
         whatIsNameInYear("Sophia", 2012, 2013, "M"); // no
         whatIsNameInYear("Sophia", 2012, 2014, "F"); // Emma
         whatIsNameInYear("Jacob", 2012, 2013, "F"); // no
         whatIsNameInYear("Jacob", 2012, 2013, "M"); // Noah
         whatIsNameInYear("Mason", 2014, 2012, "M"); // Ethan
-        */
+
         whatIsNameInYear("Susan", 1972, 2014, "F");
         whatIsNameInYear("Owen", 1974, 2014, "M");
     }
@@ -144,12 +135,10 @@ public class BabyNames {
     }
     
     public void testYearOfHighestRank() {
-        /*
         System.out.println(yearOfHighestRank("Mason", "M")); // 2012
         System.out.println(yearOfHighestRank("Olivia", "M")); // -1
         System.out.println(yearOfHighestRank("Olivia", "F")); // 2014
         System.out.println(yearOfHighestRank("Noah", "M")); // 2013
-         */
         
         System.out.println(yearOfHighestRank("Genevieve", "F"));
         System.out.println(yearOfHighestRank("Mich", "M"));
@@ -173,12 +162,10 @@ public class BabyNames {
     }
     
     public void testGetAverageRank() {
-        /*
         System.out.println(getAverageRank("Mason" , "M"));  // 3.0
         System.out.println(getAverageRank("Jacob" , "M"));  // 2.66
         System.out.println(getAverageRank("Jb" , "M"));  // -1.0
         System.out.println(getAverageRank("Mason" , "F"));  // -1.0
-        */
        
         System.out.println(getAverageRank("Susan" , "F"));
         System.out.println(getAverageRank("Robert" , "M"));
@@ -186,7 +173,6 @@ public class BabyNames {
     
     public int getTotalBirthsRankedHigher(int year, String name, String gender) {
         int total = 0;
-        // FileResource fr = new FileResource("us_babynames_test/yob" + year + "short.csv");
         FileResource fr = new FileResource("us_babynames_by_year/yob" + year + ".csv");
         for (CSVRecord rec : fr.getCSVParser(false)) {
             if (rec.get(0).equals(name) && rec.get(1).equals(gender)) {
@@ -200,11 +186,9 @@ public class BabyNames {
     }
     
     public void testGetTotalBirthsRankedHigher() {
-        /*
         System.out.println(getTotalBirthsRankedHigher(2012, "Ethan", "M")); // 15
         System.out.println(getTotalBirthsRankedHigher(2013, "Liam", "M")); // 12
-        System.out.println(getTotalBirthsRankedHigher(2013, "Liam", "F")); // 39 - выдаст всех девочек
-        */
+        System.out.println(getTotalBirthsRankedHigher(2013, "Liam", "F")); // 39 
        
        System.out.println(getTotalBirthsRankedHigher(1990, "Emily", "F"));
        System.out.println(getTotalBirthsRankedHigher(1990, "Drew", "M"));
